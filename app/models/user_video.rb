5832262e306f8078e2502bf731f8b9cbc0edae66
singjie <singjie@singjie.com>
2013-07-14 14:28:01 +0800

    Update attributes

--- app/models/user_video.rb
+++ app/models/user_video.rb
@@ -1,4 +1,6 @@
 class UserVideo < ActiveRecord::Base
   belongs_to :user
   # attr_accessible :title, :body
+  has_attached_file :video, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  
 end
